c70ff4ea308fbb833d64bcf06fe9ea180b475e78
Nhu Quynh <lequin@gmail.com>
2025-12-13 08:21:05 +0700

    cap nhat database

--- QuanLiBanGiay/QuanLiBanGiay/DBConnection.cs
+++ QuanLiBanGiay/QuanLiBanGiay/DBConnection.cs
@@ -10,9 +10,9 @@ namespace QuanLiBanGiay
 {
     public static class DBConnection
     {
-        //private static readonly string connectionString = "Data Source=ADMIN\\SQLSERVERMS;Initial Catalog=QL_BANGIAY;Integrated Security=True;TrustServerCertificate=True";
+        private static readonly string connectionString = "Data Source=ADMIN\\SQLSERVERMS;Initial Catalog=QL_BANGIAY;Integrated Security=True;TrustServerCertificate=True";
         //private static readonly string connectionString = "Data Source=HOMEPC\\MSSQLSERVER01;Initial Catalog=QL_BANGIAY;Integrated Security=True;TrustServerCertificate=True";
-        private static readonly string connectionString = "Data Source=.;Initial Catalog=QL_BANGIAY;Integrated Security=True;TrustServerCertificate=True";
+        //private static readonly string connectionString = "Data Source=.;Initial Catalog=QL_BANGIAY;Integrated Security=True;TrustServerCertificate=True";
         public static SqlConnection GetConnection()
         {
             return new SqlConnection(connectionString);
