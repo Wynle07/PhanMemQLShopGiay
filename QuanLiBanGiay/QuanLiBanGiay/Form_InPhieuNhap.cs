8a5aa7e4c548daa584f088aedaaef3a998ce10d5
Nhu Quynh <lequin@gmail.com>
2025-12-17 14:39:15 +0700

    cap nhat InPhieuNhap

---
+++ QuanLiBanGiay/QuanLiBanGiay/Form_InPhieuNhap.cs
@@ -0,0 +1,26 @@
+﻿using CrystalDecisions.CrystalReports.Engine;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuanLiBanGiay
+{
+    public partial class Form_InPhieuNhap : Form
+    {
+        public Form_InPhieuNhap()
+        {
+            InitializeComponent();
+        }
+        public void HienThiBaoCao(ReportDocument rpt)
+        {
+            crystalReportViewer1.ReportSource = rpt;
+            crystalReportViewer1.Refresh();
+        }
+    }
+}
